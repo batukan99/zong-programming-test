245ec26de998f15db74941169c17ea1cafdad9b9
Batukan Demirtaş <76396946+batukan99@users.noreply.github.com>
2024-02-11 18:33:24 +0300

    Update GabbableObject

--- Assets/Scripts/GrabbableObject.cs
+++ Assets/Scripts/GrabbableObject.cs
@@ -1,32 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zong.Sound;
+using Zong.UI;
 
 public class GrabbableObject : MonoBehaviour, IGrabbable
 {
+    #region Unity Properties
+    [field: SerializeField]
+    protected AudioSource audioSource;
+    
     [field: SerializeField]
     private Tooltip Tooltip;
+    #endregion
     public Transform GrabTransform {get; set;}
     public Rigidbody Rigidbody {get; set;}
+
+    public static event HandleObjectCollected OnObjectCollected;
+    public delegate void HandleObjectCollected(ItemData itemData);
+    public static event HandleObjectDropped OnObjectDropped;
+    public delegate void HandleObjectDropped(ItemData itemData);
+    public ItemData StoneItemData;
+    public void SetItemData() => StoneItemData = Resources.Load<ItemData>("StoneItemData");
+    
+    public bool IsPicked() => isPicked;
+    private bool isPicked = false;
     private float lerpSpeed = 10f;
 
+   
+    #region Unity Methods
     private void Awake()
     {
         Rigidbody = GetComponent<Rigidbody>();
+        SetItemData();
     }
-    public void Grab(Transform grabTransform) 
-    {
-        this.GrabTransform = grabTransform;
-        this.Rigidbody.useGravity = false;
-        this.Tooltip.gameObject.SetActive(false);
-    }
-    public void Drop() 
-    {
-        this.GrabTransform = null;
-        this.Rigidbody.useGravity = true;
-        this.Tooltip.gameObject.SetActive(true);
-    }
-
     private void FixedUpdate()
     {
         if (GrabTransform != null) 
@@ -35,4 +43,28 @@ public class GrabbableObject : MonoBehaviour, IGrabbable
             Rigidbody.MovePosition(newPosition);
         }   
     }
+    #endregion
+
+    #region Public Methods
+    public void Grab(Transform grabTransform) 
+    {
+        this.isPicked = true;
+        this.GrabTransform = grabTransform;
+        this.Rigidbody.useGravity = false;
+        this.Tooltip.gameObject.SetActive(false);
+        OnObjectCollected?.Invoke(StoneItemData);
+        Singleton<UIManager>.Instance.EnableMainPanel();
+        Singleton<SoundManager>.Instance.PlayItemPickupSound(audioSource);
+    }
+    public void Drop() 
+    {
+        this.isPicked = false;
+        this.GrabTransform = null;
+        this.Rigidbody.useGravity = true;
+        //this.Tooltip.gameObject.SetActive(true);
+        OnObjectDropped?.Invoke(StoneItemData);
+        Singleton<SoundManager>.Instance.PlayItemDropSound(audioSource);
+    }
+    #endregion
+
 }
